8ddc425453eae2b3a12d25f9749da9976781e38b
Antonio Javier Cabrera Gutierrez <javiercabrera184@gmail.com>
2019-05-08 06:49:38 +0200

    Update Program.cs

--- Extra/Program.cs
+++ Extra/Program.cs
@@ -8,11 +8,10 @@ namespace EngineIO.Samples
 {
     class Program
     {
-        //In this sample we are switching the living room light on and off 10 times.
+        
         static void Main(string[] args)
         {
-            //We are using a MemoryBit which we get from the MemoryMap.
-            //You can find all the memory addresses at the Home I/O Memory Address document.
+            
 
             do
             {
